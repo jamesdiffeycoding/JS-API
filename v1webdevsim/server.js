f4e9b36d0a154f0ed0ebf9b3b0a294d77052508d
James <jamesdiffey8@gmail.com>
2024-04-17 16:37:50 +0100

    add homepage

--- v1webdevsim/server.js
+++ v1webdevsim/server.js
@@ -5,16 +5,20 @@ const app = express()
 
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }))
-
-// allows you to access req.params
+// allows you to access req.body /
 app.use(express.json())
 
 // templating
 app.set("view engine", "ejs")
 
+const userRouter = require("./routes/users")
+
+// app.get("/",  res.send(`Get User With ID `))
+app.get("/", (req, res) => {
+    res.send(`This is the homepage`)
+})
 
 // refer to routers for different parts of your application
-const userRouter = require("./routes/users")
 app.use("/users", userRouter)
 
 app.listen(3000)
