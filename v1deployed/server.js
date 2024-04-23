c99254a18bcf5f0a0e3e985960bd5bbc7b8e6398
Sara Thampi <saraannthampi@gmail.com>
2024-04-23 17:11:23 +0100

    remove cors

--- v1deployed/server.js
+++ v1deployed/server.js
@@ -2,14 +2,14 @@ console.log("FILE READ: ' server.js'");
 
 const express = require("express");
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 // CORS middleware setup
-app.use(cors({
-  origin: '*', // Set your desired origin here or use a function for dynamic origin determination
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-}));
+// app.use(cors({
+  // origin: '*', // Set your desired origin here or use a function for dynamic origin determination
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+// }));
 
 // Other middleware setup
 app.use(express.static("public"));
