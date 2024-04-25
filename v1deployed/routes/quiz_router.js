86c11cdb6e0fdfe5a3da54e58749bd3b37d3530b
James <jamesdiffey8@gmail.com>
2024-04-25 15:16:18 +0100

    update

--- v1deployed/routes/quiz_router.js
+++ v1deployed/routes/quiz_router.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const quizControllers = require('../controllers/quiz_controllers.js')
 
 router.get("/quiz", quizControllers.getAllQuestions)
-router.get("/quiz/:id", quizControllers.getAllQuestions)
+router.get("/quiz/:id", quizControllers.getQuestionById)
 // router.get("/quiz/:id", quizControllers.getQuestonById(iid))
 
 // router.post("/quiz", quizControllers.createQuestion)
